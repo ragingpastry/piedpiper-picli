cb4b39329361641cd66d770955ebc5460990abdc
Nick Wilburn <senior.crepe@gmail.com>
2019-05-17 09:30:44 -0500

    WIP remove style test

--- tests/unit/command/test_style.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from picli.command import style
-
-def test_execute(mocker, patched_flake8, patched_style_pipe_config, config_instance):
-    styler = style.Style(config_instance)
-    styler.execute()
-
-    patched_flake8.assert_called_once_with()
